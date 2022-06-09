39eba41510eab4ca1899a1ab924e286bef65e271
choe21 <104405361+choe21@users.noreply.github.com>
2022-06-09 13:23:08 +0700

    Add files via upload

---
+++ dami/src/views/home.js
@@ -0,0 +1,105 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet'
+
+import './home.css'
+
+const Home = (props) => {
+  return (
+    <div className="home-container">
+      <Helmet>
+        <title>Digital Tidy Squid</title>
+        <meta property="og:title" content="Digital Tidy Squid" />
+      </Helmet>
+      <img
+        alt="image"
+        src="/playground_assets/0-300h.gif"
+        className="home-image"
+      />
+      <img
+        alt="image"
+        src="/playground_assets/0.1-200h.png"
+        className="home-image01"
+      />
+      <img
+        alt="pastedImage"
+        src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/5a05454a-6b1d-40c7-8896-c3f14c778b9e/8e52b9d8-dd20-440b-b1ff-1983769c72ca?org_if_sml=130775"
+      />
+      <img
+        alt="image"
+        src="/playground_assets/1.%20%C4%91i%CC%A3a%20ly%CC%81%20vn%20tg-600h.png"
+        className="home-image02"
+      />
+      <img
+        alt="image"
+        src="/playground_assets/1.1.%20%C4%91i%CC%A3a%20ly%CC%81%20vn%20ca-1000w.png"
+        className="home-image03"
+      />
+      <img
+        alt="pastedImage"
+        src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/5a05454a-6b1d-40c7-8896-c3f14c778b9e/ea4eb08c-5fb3-4a36-a25f-00f5f2002946?org_if_sml=1431780"
+      />
+      <img
+        alt="image"
+        src="/playground_assets/tit1-300h.png"
+        className="home-image04"
+      />
+      <img
+        alt="image"
+        src="/playground_assets/1.3.%20khu%20tro%CC%82%CC%80ng%20lu%CC%81a-1000w.png"
+        className="home-image05"
+      />
+      <img
+        alt="pastedImage"
+        src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/5a05454a-6b1d-40c7-8896-c3f14c778b9e/2f57b41e-6a34-4f79-a4fa-ab78ad1cac2b?org_if_sml=1179242"
+      />
+      <img
+        alt="image"
+        src="/playground_assets/tit2-300h.png"
+        className="home-image06"
+      />
+      <img
+        alt="pastedImage"
+        src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/5a05454a-6b1d-40c7-8896-c3f14c778b9e/b41d7569-0b3c-4ac4-b666-a3e022347fb1?org_if_sml=154690"
+      />
+      <img
+        alt="image"
+        src="/playground_assets/tit3-300h.png"
+        className="home-image07"
+      />
+      <img
+        alt="pastedImage"
+        src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/5a05454a-6b1d-40c7-8896-c3f14c778b9e/28313966-6b04-49f8-bd85-c0b111fe5a1a?org_if_sml=186992"
+      />
+      <img
+        alt="image"
+        src="/playground_assets/tit4-300h.png"
+        className="home-image08"
+      />
+      <img
+        alt="pastedImage"
+        src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/5a05454a-6b1d-40c7-8896-c3f14c778b9e/fac6954f-ed50-4b92-9284-462056a10b10?org_if_sml=1226009"
+      />
+      <img
+        src="/playground_assets/tit5-300h.png"
+        alt="image"
+        className="home-image09"
+      />
+      <img
+        alt="pastedImage"
+        src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/5a05454a-6b1d-40c7-8896-c3f14c778b9e/256abbcf-7e32-456a-aa1f-8b3211633844?org_if_sml=155502"
+      />
+      <img
+        src="/playground_assets/tit6-300h.png"
+        alt="image"
+        className="home-image10"
+      />
+      <img
+        alt="pastedImage"
+        src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/5a05454a-6b1d-40c7-8896-c3f14c778b9e/07aba8b8-d184-4de4-92c8-35798f43e1d4?org_if_sml=180105"
+      />
+    </div>
+  )
+}
+
+export default Home
